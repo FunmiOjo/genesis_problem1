4756f1d3bc030999ca08af6a608fa96a8fc8ed55
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2019-02-05 15:36:31 -0500

    Added comments

--- check_words.py
+++ check_words.py
@@ -8,6 +8,7 @@ from get_dictionary import create_dictionary_hash_table, create_complete_diction
 def setup():
   create_complete_dictionary()
 
+# removes the character at the given index and returns the resulting word
 def splice(word, index):
   if index == 0:
     return word[1:]
@@ -19,6 +20,7 @@ def splice(word, index):
 def is_in_dictionary(word):
   return word in dictionary_table
 
+# recursively checks whether words and subwords are in dictionary
 def check_word_permutations(word):
   if len(word) == 0:
     return True
@@ -32,6 +34,8 @@ def check_word_permutations(word):
         return True
     return False
 
+# checks to find first word that satisfies requirements. this word will be the longest
+# because the list of words is sorted from longest to shortest
 def find_valid_word():
   for i in range(len(sorted_words)):
     print('Checking {0} (length: {1})'.format(sorted_words[i], len(sorted_words[i])))
@@ -39,6 +43,7 @@ def find_valid_word():
       return sorted_words[i]
   return 'No valid words'
 
+# checks to see whether the Scrabble dictionary has already been downloaded
 def dictionary_exists():
   for files in os.walk(os.getcwd()):
     if 'sowpods.txt' in files:
@@ -48,7 +53,7 @@ def dictionary_exists():
 if not dictionary_exists():
   setup()
   
-sorted_words = sort_words()
+sorted_words = sort_words() # list of dictionary words sorted from longest to shortest
 dictionary_table = create_dictionary_hash_table()
 print(find_valid_word())
 
